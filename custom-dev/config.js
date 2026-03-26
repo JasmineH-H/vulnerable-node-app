1ba3ab082fb16339283d95e7b34076630c590351
SharonHuang <s89huang@uwaterloo.ca>
2026-03-25 18:25:32 -0700

    test 1

--- custom-dev/config.js
+++ custom-dev/config.js
@@ -1,2 +1,8 @@
-const password = "secret123";     // HARDCODED_SECRET
-eval("console.log('danger')");    // INSECURE_FUNCTION
+// HARDCODED_SECRET
+const password = "secret123";     
+
+// INSECURE_FUNCTION
+eval("console.log('danger')");    
+
+// SQL_INJECTION 
+db.query("SELECT * FROM users WHERE id = " + userId);
